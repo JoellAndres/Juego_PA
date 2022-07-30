6eef5433af1696fc9fb89218adcfaf7159359103
JoellAndres <joeltuma278@gmail.com>
2022-07-30 19:09:40 -0300

    Juego terminado con vidas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject menuPrincipal;
+    public GameObject menuGameOver;
+    public GameObject col;
     public Renderer fondo;
-
+    public GameObject piedra1;
+    public GameObject piedra2;
+    public List<GameObject> cols;
+    public List<GameObject> obstaculos;
+    public float velocidad = 4;
+    public bool gameOn;
+    public bool start;
+    private Animator animator;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!start)
+        {
+            gameOn = false;
+        }
+        else
+        {
+            gameOn = true;
+        }
+            
+
+        //Crear mapa
+        for (int i = 0; i < 21; i++)
+        {
+            cols.Add(Instantiate(col, new Vector2(-10 + i, -3), Quaternion.identity)); 
+        }
+
+
+        //Crear piedras
+        obstaculos.Add(Instantiate(piedra1, new Vector2(14, -2), Quaternion.identity));
+        obstaculos.Add(Instantiate(piedra2, new Vector2(18, -2), Quaternion.identity));        
     }
 
     // Update is called once per frame
     void Update()
     {
-        fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.04f, 0) * Time.deltaTime;
+        
+        if ((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            menuPrincipal.SetActive(false);
+            start = true;
+            gameOn = true;
+        }
+
+        if (start)
+        {
+        
+            if (gameOn)
+            {
+                fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.11f, 0) * Time.deltaTime;
+
+                        
+                //Mover Mapa
+                for(int i=0; i<cols.Count; i++)
+                {
+
+                    if (cols[i].transform.position.x <= -10)
+                    {
+                        cols[i].transform.position = new Vector3(10, -3,0);
+                    }
+
+
+                    cols[i].transform.position = cols[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * velocidad;
+                }
+
+                //Mover Obstaculos
+                for(int i=0; i<obstaculos.Count; i++)
+                {
+                    if (obstaculos[i].transform.position.x <= -10)
+                    {
+                        float randomObstaculo = Random.Range(11, 18);
+                        obstaculos[i].transform.position = new Vector3(randomObstaculo, -2, 0);
+                    }
+
+                    obstaculos[i].transform.position = obstaculos[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * velocidad;
+                }
+            }
+            else
+            {
+                //Muestro mensaje cuando el jugador muere
+                menuGameOver.SetActive(true);
+
+                //Reiniciar juego cuando el jugador muere
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SceneManager.LoadScene("SampleScene");
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,38 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Jugador : MonoBehaviour
 {
     public float fuerzaSalto;
-    private Rigidbody2D rigidbody2D;
+    public Rigidbody2D rigidbody2D;
     private Animator animator;
+    public GameManager gameManager;
+    public int contador_vidas = 3;
+    public GameObject[] hearts;
 
     // Start is called before the first frame update
     void Start()
     {
 
         animator = GetComponent<Animator>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
-
+        //rigidbody2D = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        if (gameManager.gameOn) 
+        { 
+            if((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.UpArrow)))
+            {
+                UpdateState("Saltar");
+                rigidbody2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+                
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                rigidbody2D.AddForce(Vector2.down * fuerzaSalto, ForceMode2D.Impulse);
+            }
+            
+        }
+
+        //Borro el corazón de la vida en el juego
+        if(contador_vidas < 3)
         {
-            animator.SetBool("estaSaltando", true);
-            rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
+            Destroy(hearts[contador_vidas]);
+        }
+        
+
+    }
+
+    public void UpdateState(string state = null)
+    {
+        if (state != null)
+        {
+            animator.Play(state);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Suelo")
-        {
-            animator.SetBool("estaSaltando", false);
+        if (gameManager.gameOn)
+        {        
+            if(collision.gameObject.tag == "Suelo")
+            {               
+                UpdateState("Correr");
+            }
+
+            if(collision.gameObject.tag == "Obstaculo")
+            {
+
+                contador_vidas--;
+
+                if(contador_vidas <= 0)
+                {
+                    UpdateState("Morir");
+                    rigidbody2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+                    gameManager.gameOn = false;
+                    GetComponent<BoxCollider2D>().enabled = false;
+                }
+                
+            }
         }
     }
 }
